4f3d1f99ab4c549abb279eafc901a6484eff5117
A/C <academiadecodigo@Miguels-Mini.lan>
2024-05-24 15:43:57 +0100

    adds a initial sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hey, its PIGGY BANK!!!!");
         PiggyBank piggyBank = new PiggyBank();
 
         piggyBank.setBalance(100);
